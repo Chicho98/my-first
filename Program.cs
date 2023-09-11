e5fae69b827b5eb40add31ac58de59855dfe34e5
Chicho98 <leecordeiro99@gmail.com>
2023-09-11 19:33:52 -0300

    Se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.Write("me quiero comer un shimmy baby");
+Console.WriteLine("1  maizenita mas!");
+Console.Beep();
+//comentario
+Console.Write("shimmy\nbaby");
+Console.ReadKey();
+/*primero qe nada, backslash qe es la barra al revez "\" se hace con "alt+92" y por ahora solo se
+qe sirve para para mezclarla con una b O t para meterle un tab y alejar un texto de otro en la consola
+o para hacercar un texto de otro de la consola, console.write("") sirve para meter un texto en la consola
+console.writeline hace lo mismo qe lo anterior pero le da un renglon abajo el texto
+console.beep en teoria hace un beep al final de la ejecucion de la consola pero no logre hacerlo funcionar
+ barra barra sirve para hacer comentarios muy useful 
+console.readkey la consola espera a qe toqes una tecla para terminar el proceso esteticamente util para no ver todo ese
+*/
+
